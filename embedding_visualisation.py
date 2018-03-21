6c589687fc5c46417d5a6419261a545438e265e7
Nils Schaetti <n.schaetti@gmail.com>
2018-03-21 10:23:29 +0100

    Embedding visualization

---
+++ embedding_visualisation.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# File : core.classifiers.RCNLPTextClassifier.py
+# Description : Echo State Network for text classification.
+# Auteur : Nils Schaetti <nils.schaetti@unine.ch>
+# Date : 01.02.2017 17:59:05
+# Lieu : Nyon, Suisse
+#
+# This file is part of the Reservoir Computing NLP Project.
+# The Reservoir Computing Memory Project is a set of free software:
+# you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Foobar is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import argparse
+import torch
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.optim as optim
+from modules import LanguageModel
+from torch.utils.data import DataLoader
+import datasets
+import numpy as np
+
+
+####################################################
+# Main function
+####################################################
+
+
+# Argument parser
+parser = argparse.ArgumentParser(description="Character embedding visualization")
+
+# Argument
+parser.add_argument("--input", type=str, help="Embedding input file")
+parser.add_argument("--output", type=str, help="Output image file")
+parser.add_argument("--image-size", type=int, help="Image size", default=4000)
+args = parser.parse_args()
+
+# Load
+token_to_ix, weights = torch.load(open(args.input, 'wb'))
+
+# Embedding matrix
+embedding = weights.numpy()
+print(embedding.size())
