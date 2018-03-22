a09c1eda10bbc6b24fe2f28a7b77a89094467c46
Nils Schaetti <n.schaetti@gmail.com>
2018-03-22 20:36:39 +0100

    Embedding visualisation

--- embedding_visualisation.py
+++ embedding_visualisation.py
@@ -31,7 +31,8 @@ import matplotlib.pyplot as plt
 
 # Settings
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
-            'v', 'x', 'y', 'z', '.', ',', ';', ':', '-', '!', '?', '"', '\'', '(', ')', ' ']
+            'v', 'x', 'y', 'z', '.', ',', ';', ':', '-', '!', '?', '"', '\'', '(', ')', ' ', '0', '1', '2', '3', '4',
+            '5', '6', '7', '8', '9']
 
 # Argument parser
 parser = argparse.ArgumentParser(description="Character embedding visualization")
