b00567557a2660b7e7be04473f719ae38767eed1
Nils Schaetti <n.schaetti@gmail.com>
2018-03-22 12:20:17 +0100

    Maximum sample size

--- embedding_visualisation.py
+++ embedding_visualisation.py
@@ -51,6 +51,8 @@ args = parser.parse_args()
 token_to_ix, weights = torch.load(open(args.input, 'rb'))
 
 # Embedding layer
+weights = weights.cpu()
+print(type(weights))
 embedding = nn.Embedding(weights.data.size(0), weights.data.size(1))
 embedding.weight = nn.Parameter(weights.data)
 
